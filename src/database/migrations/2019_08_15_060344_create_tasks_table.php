453d36c4087e098cdf2c34bf783ba528f755b1bc
sendai-itce003 <sorodoraper@01.email.com>
2019-08-15 15:21:32 +0900

    名前の列を追加

--- src/database/migrations/2019_08_15_060344_create_tasks_table.php
+++ src/database/migrations/2019_08_15_060344_create_tasks_table.php
@@ -14,7 +14,9 @@ class CreateTasksTable extends Migration
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
+
             $table->bigIncrements('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
