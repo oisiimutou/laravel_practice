5212e908a586f50bfe195f65ed73dfd2f63235e9
sendai-itce003 <sorodoraper@01.email.com>
2019-08-16 13:49:06 +0900

    タスクを表示させた

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -3,8 +3,13 @@
     <head>
         <title>Laravel Quickstart - Basic</title>
 
-        <!-- CSSとJavaScript -->
+        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     </head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UACompatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initital-scale=1.0">
 
     <body>
         <div class="container">
@@ -14,5 +19,8 @@
         </div>
 
         @yield('content')
+
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/jpuery/2.1.4/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     </body>
 </html>
