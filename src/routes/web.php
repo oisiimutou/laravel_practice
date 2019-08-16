3fbadb860b2c807de8bed76c66a7e3ff8666f775
sendai-itce003 <sorodoraper@01.email.com>
2019-08-16 10:33:41 +0900

    エラーが出るよう設定した。

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,4 +1,7 @@
 <?php
+use App\Task;
+use Illuminate\Http\Request;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +21,16 @@ Route::get('/', function () {
 });
 
 Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')->withInput()->withErrors($validator);
+    };
+
+    $task =new Task;
+    $task->name = $request->name;
+    $task->save();
     return redirect('/');
 });
